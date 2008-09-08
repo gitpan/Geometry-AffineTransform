07885cd7f8eea2a3679b656d226b2f02e44c532f
Marc Liyanage <reg.cpan@entropy.ch>
2008-09-08 09:35:40 +0000

    Import of LIYANAGE/Geometry-AffineTransform-1.1 from CPAN.
    gitpan-cpan-distribution: Geometry-AffineTransform
    gitpan-cpan-version:      1.1
    gitpan-cpan-path:         LIYANAGE/Geometry-AffineTransform-1.1.tar.gz
    gitpan-cpan-author:       LIYANAGE
    gitpan-cpan-maturity:     released

--- lib/Geometry/AffineTransform.pm
+++ lib/Geometry/AffineTransform.pm
@@ -1,31 +1,31 @@
 package Geometry::AffineTransform;
 
+our $VERSION = '1.1';
+
 use strict;
 use warnings;
 
 use Carp;
 use Hash::Util;
-use Data::Dumper;
-use Math::Trig;
+use Math::Trig ();
 
-our $VERSION = '1.0';
-
+# $Id: AffineTransform.pm 390 2008-09-08 09:26:05Z liyanage $
 
 =head1 NAME
 
-Geometry::AffineTransform - Maps 2D coordinates to other 2D coordinates
+Geometry::AffineTransform - Affine Transformation to map 2D coordinates to other 2D coordinates
 
 =head1 SYNOPSIS
 
-	use Geometry::AffineTransform;
-	
-	my $t = Geometry::AffineTransform->new();
-	$t->translate($delta_x, $delta_y);
-	$t->rotate($degrees);
-	my $t2 = Geometry::AffineTransform->new()->scale(3.1, 2.3);
-	$t->concatenate($t2);
-	my ($x1, $y1, $x2, $y2, ...) = $t->transform($x1, $y1, $x2, $y2, ...);
-	
+    use Geometry::AffineTransform;
+
+    my $t = Geometry::AffineTransform->new();
+    $t->translate($delta_x, $delta_y);
+    $t->rotate($degrees);
+    my $t2 = Geometry::AffineTransform->new()->scale(3.1, 2.3);
+    $t->concatenate($t2);
+    my ($x1, $y1, $x2, $y2, ...) = $t->transform($x1, $y1, $x2, $y2, ...);
+
 =head1 DESCRIPTION
 
 Geometry::AffineTransform instances represent 2D affine transformations
@@ -39,11 +39,11 @@ transformation on one or more x/y coordinate pairs with L</transform>.
 The state of a newly created instance represents the identity transform,
 that is, it transforms all input coordinates to the same output coordinates.
 
-Most methods return the instance so that you can chain several calls:
+Most methods return the instance so that you can chain method calls:
 
     my $t = Geometry::AffineTransform->new();
     $t->scale(...)->translate(...)->rotate(...);
-    
+
     ($x, $y) = Geometry::AffineTransform->new()->rotate(..)->transform($x, $y);
 
 =cut
@@ -55,17 +55,14 @@ Most methods return the instance so that you can chain several calls:
 
 Constructor, returns a new instance configured with an identity transform.
 
-=head3 Parameters
-
-You can optionally supply any of the six specifiable parts of the transformation matrix
-if you want an initial state different from the identity transform:
+You can optionally supply any of the six specifiable parts of the transformation matrix.
+The six values in the first two colums are the specifiable values:
 
     [ m11 m21 0 ]
     [ m21 m22 0 ]
     [ tx  ty  1 ]
 
-The six values in the first two colums are the specifiable values. You can initialize
-them with key/value parameters:
+The constructor lets you initialize them with key/value parameters:
 
     my $t = Geometry::AffineTransform->new(tx => 10, ty => 15);
 
@@ -75,19 +72,49 @@ By default, the identity transform represented by this matrix is used:
     [ 0 1 0 ]
     [ 0 0 1 ]
 
+In other words, invoking the constructor without arguments is equivalent to this:
+
+    my $t = Geometry::AffineTransform->new(
+        m11 => 1,
+        m12 => 0,
+        m21 => 0,
+        m22 => 1,
+        tx  => 10,
+        ty  => 15
+    );
+
 =cut
 
 sub new {
 	my $self = shift @_;
 	my (%args) = @_;
-	
+
 	my $class = ref($self) || $self;
 	$self = bless {m11 => 1, m12 => 0, m21 => 0, m22 => 1, tx => 0, ty => 0, %args}, $class;
+#	$self->init();
 	Hash::Util::lock_keys(%$self);
-	
+
 	return $self;
 }
 
+
+# hook for subclasses
+# sub init {
+# }
+
+
+
+
+=head2 clone
+
+Returns a clone of the instance.
+
+=cut
+
+sub clone {
+	my $self = shift @_;
+	return $self->new()->set_matrix_2x3($self->matrix_2x3());
+}
 
 
 
@@ -95,12 +122,8 @@ sub new {
 
 Transform one or more coordinate pairs according to the current state.
 
-=head3 Parameters
-
 This method expects an even number of positional parameters, each pair
 representing the x and y coordinates of a point.
-
-=head3 Result
 
 Returns the transformed list of coordinates in the same form as the input list.
 
@@ -109,14 +132,14 @@ Returns the transformed list of coordinates in the same form as the input list.
 sub transform {
 	my $self = shift;
 	my (@pairs) = @_;
-	
+
 	my @result;
 	while (my ($x, $y) = splice(@pairs, 0, 2)) {
 		my $x2 = $self->{m11} * $x + $self->{m21} * $y + $self->{tx};
 		my $y2 = $self->{m12} * $x + $self->{m22} * $y + $self->{ty};
 		push @result, $x2, $y2;
 	}
-	
+
 	return @result;
 }
 
@@ -138,13 +161,9 @@ sub concatenate_matrix_2x3 {
 
 Combine the receiver's state with that of another transformation instance.
 
-=head3 Parameters
-
 This method expects a list of one or more C<Geometry::AffineTransform>
 instances and combines the transformation of each one with the receiver's
 in the given order.
-
-=head3 Result
 
 Returns C<$self>.
 
@@ -152,17 +171,18 @@ Returns C<$self>.
 
 sub concatenate {
 	my $self = shift;
-	my ($t) = @_;
-	croak "Expecting argument of type Geometry::AffineTransform" unless (ref $t);
-	return $self->concatenate_matrix_2x3($t->matrix_2x3());
+	my @transforms = @_;
+	foreach my $t (@transforms) {
+		croak "Expecting argument of type Geometry::AffineTransform" unless (ref $t);
+		$self->concatenate_matrix_2x3($t->matrix_2x3()) ;
+	}
+	return $self;
 }
 
 
 =head2 scale
 
 Adds a scaling transformation.
-
-=head3 Parameters
 
 This method expects positional parameters.
 
@@ -178,8 +198,6 @@ The scaling factor for the y dimension.
 
 =back
 
-=head3 Result
-
 Returns C<$self>.
 
 =cut
@@ -191,12 +209,10 @@ sub scale {
 }
 
 
-=head2 scale
+=head2 translate
 
 Adds a translation transformation, i.e. the transformation shifts
 the input coordinates by a constant amount.
-
-=head3 Parameters
 
 This method expects positional parameters.
 
@@ -211,8 +227,6 @@ The offset for the x dimension.
 The offset for the y dimension.
 
 =back
-
-=head3 Result
 
 Returns C<$self>.
 
@@ -231,8 +245,6 @@ sub translate {
 
 Adds a rotation transformation.
 
-=head3 Parameters
-
 This method expects positional parameters.
 
 =over
@@ -244,8 +256,6 @@ positive values rotate counterclockwise.
 
 =back
 
-=head3 Result
-
 Returns C<$self>.
 
 =cut
@@ -253,7 +263,7 @@ Returns C<$self>.
 sub rotate {
 	my $self = shift;
 	my ($degrees) = @_;
-	my $rad = deg2rad($degrees);
+	my $rad = Math::Trig::deg2rad($degrees);
 	return $self->concatenate_matrix_2x3(cos($rad), sin($rad), -sin($rad), cos($rad), 0, 0);
 }
 
@@ -298,18 +308,18 @@ sub matrix {
 sub matrix_multiply {
 	my $self = shift;
 	my ($a, $b) = @_;
-	
+
 # 	a11 a12 0
 # 	a21 a22 0
 # 	a31 a32 1
-# 	
+#
 # 	b11 b12 0
 # 	b21 b22 0
 # 	b31 b32 1
 
 	my ($a11, $a12, $a21, $a22, $a31, $a32) = @$a;
 	my ($b11, $b12, $b21, $b22, $b31, $b32) = @$b;
-	
+
 	return
 		($a11 * $b11 + $a12 * $b21),        ($a11 * $b12 + $a12 * $b22),
 		($a21 * $b11 + $a22 * $b21),        ($a21 * $b12 + $a22 * $b22),
@@ -319,38 +329,37 @@ sub matrix_multiply {
 }
 
 
-
-
-
-
-
-
-1;
-
 =head1 SEE ALSO
 
 =over
 
 =item Apple Quartz 2D Programming Guide - The Math Behind the Matrices
 
-http://developer.apple.com/documentation/GraphicsImaging/Conceptual/drawingwithquartz2d/dq_affine/chapter_6_section_7.html
+L<http://developer.apple.com/documentation/GraphicsImaging/Conceptual/drawingwithquartz2d/dq_affine/chapter_6_section_7.html>
 
 =item Sun Java java.awt.geom.AffineTransform
 
-http://java.sun.com/j2se/1.4.2/docs/api/java/awt/geom/AffineTransform.html
+L<http://java.sun.com/j2se/1.4.2/docs/api/java/awt/geom/AffineTransform.html>
 
 =item Wikipedia - Matrix Multiplication
 
-http://en.wikipedia.org/wiki/Matrix_(mathematics)#Matrix_multiplication
+L<http://en.wikipedia.org/wiki/Matrix_(mathematics)#Matrix_multiplication>
 
 =back
 
 
 
+=head1 AUTHOR
 
+Marc Liyanage <liyanage@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
 Copyright 2008 Marc Liyanage.
 
+Distributed under the Artistic License 2.
+
 =cut
+
+
+1;
